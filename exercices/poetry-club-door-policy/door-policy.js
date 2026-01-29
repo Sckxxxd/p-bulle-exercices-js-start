ff1e19c783c55cdab75d1894dad911256520ad97
Sélim Chouk <pw18wvn@eduvaud.ch>
2026-01-29 14:20:41 +0100

    chore(door-policy.js) : Exercice terminé [30] [DONE]

--- exercices/poetry-club-door-policy/door-policy.js
+++ exercices/poetry-club-door-policy/door-policy.js
@@ -27,7 +27,7 @@
  * @returns {string}
  */
 export function frontDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  return line[0];
 }
 
 /**
@@ -38,7 +38,8 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
 
 /**
@@ -49,7 +50,12 @@ export function frontDoorPassword(word) {
  * @returns {string}
  */
 export function backDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+ 
+  line = line.trim();
+  let i = line.length - 1;
+
+  return line[i];
+
 }
 
 /**
@@ -60,5 +66,7 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  word = word[0].toUpperCase() + word.slice(1).toLowerCase();
+  word += ", please";
+  return word;
 }
