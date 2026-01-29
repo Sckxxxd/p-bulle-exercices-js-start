05ba8e62bd344c156db0534606dbad542bc43656
Sélim Chouk <pw18wvn@eduvaud.ch>
2026-01-29 15:18:46 +0100

    fix(annalyns-infiltration) exerice corrigé [10] [DONE]

--- exercices/annalyns-infiltration/annalyns-infiltration.js
+++ exercices/annalyns-infiltration/annalyns-infiltration.js
@@ -54,6 +54,7 @@ export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
  * @returns {boolean} Whether or not you can send a signal to the prisoner.
  */
 export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
+  
     return !archerIsAwake && prisonerIsAwake;
 }
 
