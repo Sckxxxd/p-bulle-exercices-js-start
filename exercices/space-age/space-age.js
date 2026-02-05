6a466930cd0eda7f32e2477c8ec206f0c6950f24
Sélim Chouk <pw18wvn@eduvaud.ch>
2026-02-05 15:34:13 +0100

    chore(space-age.js) Exercice terminé [30] [DONE]

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,26 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+const EARTH_YEAR_IN_SECONDS = 31557600;
+
+const ORBITAL_PERIODS = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1.0,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
 };
+
+export const age = (planet, seconds) => {
+  if (!ORBITAL_PERIODS.hasOwnProperty(planet)) {
+    throw new Error("not a planet");
+  }
+
+  const orbitalPeriod = ORBITAL_PERIODS[planet];
+  const result = seconds / (EARTH_YEAR_IN_SECONDS * orbitalPeriod);
+  return Number(result.toFixed(2));
+};
+
