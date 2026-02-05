b02a690064e0e98b052c440b20204abd4f678f6e
Sélim Chouk <pw18wvn@eduvaud.ch>
2026-02-05 14:22:39 +0100

    chore(enchantement.js) Exercice terminé [25] [DONE]

--- exercices/elyses-looping-enchantments/enchantments.js
+++ exercices/elyses-looping-enchantments/enchantments.js
@@ -9,8 +9,13 @@
  * @returns {number} number of cards of a single type there are in the deck
  */
 export function cardTypeCheck(stack, card) {
-  // 🚨 Use .forEach
-  throw new Error('Implement the cardTypeCheck function');
+  let count = 0;
+  stack.forEach((value) => {
+    if (value === card) {
+      count++;
+    }
+  });
+  return count;
 }
 
 /**
@@ -21,6 +26,12 @@ export function cardTypeCheck(stack, card) {
  * @returns {number} number of cards that are either odd or even (depending on `type`)
  */
 export function determineOddEvenCards(stack, type) {
-  // 🚨 Use a `for...of` loop
-  throw new Error('Implement the determineOddEvenCards function');
+  let count = 0;
+  for (const value of stack) {
+    const isEven = value % 2 === 0;
+    if ((type && isEven) || (!type && !isEven)) {
+      count++;
+    }
+  }
+  return count;
 }
